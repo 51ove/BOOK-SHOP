a5430c11724160f756fe9a2b0abb466ea8f58322
이예은 <2yeeun@gmail.com>
2026-02-11 21:31:56 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// express 모듈
+const express = require('express');
+const app = express();
+
+// dotenv 모듈
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.listen(process.env.PORT);
+
+const userRouter = require('./routes/users.js')
+const likeRouter = require('./routes/likes.js')
+const cartRouter = require('./routes/carts.js')
+const orderRouter = require('./routes/orders.js')
+const bookRouter = require('./routes/books.js')
+
+app.use("/users", userRouter);
+app.use("/likes", likeRouter);
+app.use("/carts", cartRouter);
+app.use("/orders", orderRouter);
+app.use("/books", bookRouter);
