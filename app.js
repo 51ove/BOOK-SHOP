3ef857157e5291562adb158cac4726c857b90cb7
이예은 <2yeeun@gmail.com>
2026-02-25 23:57:42 +0900

    paging

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ dotenv.config();
 
 app.listen(process.env.PORT);
 
-const userRouter = require('./routes/users.js')
-const likeRouter = require('./routes/likes.js')
-const cartRouter = require('./routes/carts.js')
-const orderRouter = require('./routes/orders.js')
-const bookRouter = require('./routes/books.js')
+const userRouter = require('./routes/users.js');
+const likeRouter = require('./routes/likes.js');
+const cartRouter = require('./routes/carts.js');
+const orderRouter = require('./routes/orders.js');
+const bookRouter = require('./routes/books.js');
+const categoryRouter = require('./routes/category.js');
 
 app.use("/users", userRouter);
 app.use("/likes", likeRouter);
 app.use("/carts", cartRouter);
 app.use("/orders", orderRouter);
 app.use("/books", bookRouter);
+app.use("/category", categoryRouter);
