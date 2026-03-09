2cdf8dd856ac7277b13cf07a60c616f786d32c7c
이예은 <2yeeun@gmail.com>
2026-03-09 18:09:19 +0900

    order

---
+++ controller/OrderController.js
@@ -0,0 +1,123 @@
+// const conn = require('../mariadb'); // db 모듈
+const mysql = require('mysql2/promise');
+
+const {StatusCodes} = require('http-status-codes')
+
+const order = async (req,res)=>{
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        port: 3307,
+        database: 'BookShop',
+        dateStrings : true
+        });
+
+    let {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
+    
+    let delivery_id;
+    let order_id;
+
+    // delivery insert
+    let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)`;
+
+    let values = [delivery.address, delivery.receiver, delivery.contact];
+
+    let [results] = await conn.execute(sql,values)
+    delivery_id = results.insertId
+
+    // order insert
+    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+            VALUES (?, ?, ?, ?, ?)`;
+    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+
+    [results] = await conn.execute(sql,values);
+    order_id = results.insertId;
+
+
+
+    // items(장바구니 선택된 항목인 cartItemId 배열)를 가지고, 
+    // 장바구니에서 book_id, quantity 조회
+    sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`
+    let [orderItems, fields] = await conn.query(sql, [items]);
+
+    // orderedBook insert
+    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`
+    
+    // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) >
+    values = [];
+    orderItems.forEach((item)=>{
+        values.push([order_id, item.book_id, item.quantity]); // 2차원 배열로
+    });
+
+    [results] = await conn.query(sql,[values]); 
+
+    // 장바구니에서 선택된 항목 제거
+    [results] = await deleteCartItems(conn, items); // deleteCartItems이 async 함수이니 promise 반환. await 사용 가능
+
+
+    return res.status(StatusCodes.OK).json(results);
+
+};
+
+// 장바구니 선택된 항목 제거
+const deleteCartItems = async (conn, items) => {
+    
+    let sql = `DELETE FROM cartItems WHERE id IN (?)`;
+
+    let result = await conn.query(sql, [items]);
+    return result;
+}
+
+
+
+
+const getOrders = async (req,res)=>{
+
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        port: 3307,
+        database: 'BookShop',
+        dateStrings : true
+        });
+    
+    let sql =`SELECT orders.id, created_at, address, receiver, contact, 
+            book_title, total_quantity, total_price
+            FROM orders
+            LEFT JOIN delivery ON orders.delivery_id = delivery.id `;
+
+    let [rows, fields] = await conn.query(sql);
+
+    return res.status(StatusCodes.OK).json(rows);
+    
+};
+
+const getOrderDetail = async (req,res)=>{
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        port: 3307,
+        database: 'BookShop',
+        dateStrings : true
+        });
+
+    let {id} = req.params;
+    
+    
+    let sql =`SELECT book_id, title, author, price, quantity FROM BookShop.orderedBook
+            LEFT JOIN books ON orderedBook.book_id = books.id
+            WHERE order_id = ?`;
+
+    let [rows, fields] = await conn.query(sql, id);
+
+    return res.status(StatusCodes.OK).json(rows);
+};
+
+module.exports = {
+    order,
+    getOrders,
+    getOrderDetail
+}
--- data.sql
+++ data.sql
@@ -41,3 +41,38 @@ DELETE FROM cartItems WHERE id = ?;
 SELECT * FROM BookShop.cartItems
 WHERE user_id = 1
 AND id IN (2,3);
+
+-- 주문하기
+-- 배송 정보 입력
+INSERT INTO delivery (address, receiver, contact) VALUES ("서울시 중구", "이예은", "010-1234-5678");
+
+-- 주문 정보 입력
+INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id);
+VALUES ("어린왕자", 3, 60000, 1, delivery_id);
+
+-- 주문 상세 정보 입력
+INSERT INTO orderedBook (order_id, book_id, quantity) 
+VALUES (1,1,1);
+
+INSERT INTO orderedBook (order_id, book_id, quantity) 
+VALUES (1,3,2);
+
+--
+SELECT max(id) FROM BookShop.orderedBook;
+
+SELECT last_insert_id();
+
+
+-- 결제된 도서 장바구니 삭제
+DELETE FROM cartItems WHERE id IN (1,2,3);
+
+
+-- 주문 목록 조회
+SELECT orders.id, book_title, total_quantity, total_price, created_at, address, receiver, contact 
+FROM orders
+LEFT JOIN delivery ON orders.delivery_id = delivery.id 
+
+-- 주문 상세 조회
+SELECT book_id, title, author, price, quantity FROM BookShop.orderedBook
+LEFT JOIN books ON orderedBook.book_id = books.id
+WHERE order_id = ?
